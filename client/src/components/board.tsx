52aedd47a8bedfe065837da808b9bae9f9a3f5ac
marsen <marsenzakirov11@gmail.com>
2022-09-21 01:57:37 +0300

    Add board component

--- client/src/components/board.tsx
+++ client/src/components/board.tsx
@@ -1,16 +1,44 @@
 import type { NextComponentType } from "next";
-import { useState } from "react";
+import { useEffect } from "react";
+import { useRecoilState } from "recoil";
 import Cell from "./cell";
+import { boardStore } from "../store/atom";
 const Board: NextComponentType = () => {
-  const [figures, setFigures] = useState([...Array(64)]);
-  console.log();
+  const [board, setBoard] = useRecoilState(boardStore);
+
+  const initialBoard = () => {
+    let myArr = [...Array(64)];
+    myArr[0] = myArr[7] = "RB";
+    myArr[1] = myArr[6] = "KnB";
+    myArr[2] = myArr[5] = "BB";
+    myArr[3] = "QB";
+    myArr[4] = "KiB";
+
+    myArr[63] = myArr[56] = "RW";
+    myArr[63 - 1] = myArr[56 + 1] = "KnW";
+    myArr[63 - 2] = myArr[56 + 2] = "BW";
+    myArr[63 - 3] = "QW";
+    myArr[56 + 3] = "KiW";
+    for (let i = 0; i < 8; i++) {
+      myArr[8 + i] = "PB";
+      myArr[55 - i] = "PW";
+    }
+    return myArr;
+  };
+  useEffect(() => {
+    setBoard(initialBoard());
+  }, []);
   return (
     <div className="w-[calc(64px*8)] h-[calc(64px*8)] flex flex-wrap">
-      {figures.map((item, index) => {
+      {board.map((item, index) => {
         if (digit(parseInt(index.toString(8))) % 2 == 0) {
-          return <Cell isWhite={true} />;
+          return (
+            <Cell isWhite={true} key={index} figure={item} index={index} />
+          );
         } else {
-          return <Cell isWhite={false} />;
+          return (
+            <Cell isWhite={false} key={index} figure={item} index={index} />
+          );
         }
       })}
     </div>
