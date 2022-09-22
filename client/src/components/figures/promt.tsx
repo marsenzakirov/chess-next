d5550e2eaf6477d02ce91e5ad40f1c410c1bcefb
marsen <marsenzakirov11@gmail.com>
2022-09-22 20:56:57 +0300

    Change structure in promt component

--- client/src/components/figures/promt.tsx
+++ client/src/components/figures/promt.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
 const Promt = () => {
-  return <Image src="/icons/promt.svg" alt="" width={20} height={20} />;
+  return (
+    <div className="absolute">
+      <Image src="/icons/promt.svg" alt="" width={20} height={20} />
+    </div>
+  );
 };
 
 export default Promt;
